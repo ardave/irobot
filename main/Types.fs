7a7dcfd05da477e5e59000cfb51a69cd3017faa1
Falkner, David <David.B.Falkner@nordstrom.com>
2017-10-24 21:49:54 -0700

    Some initial type definitions

---
+++ main/Types.fs
@@ -0,0 +1,93 @@
+module Types
+
+type OperatingMode =
+| Off
+| Passive
+| Safe
+| Full
+
+type CommandData = {
+    OpCode: int
+    DataBytes: int
+}
+
+// Pg 6 at the top suggests that this type of command might be 
+// different from the CleaningModeCommand
+type Command =
+| Start
+| Reset
+| Stop
+| Baud
+
+type CleaningMode =
+| Spot
+| Clean
+| SeekDock
+| Power
+| Max
+| Schedule
+| SetDayTime
+
+type ActuatorCommand =
+| Drive
+| DriveDirect
+| DrivePwm
+| Motors
+| PwmMotors
+| LEDs
+| SchedulingLEDs
+| DigitalLEDsRaw
+| Buttons
+| DigitLEDsASCII
+| Song
+| Play 
+
+type InputCommand =
+| Sensors
+| QueryList
+| Stream
+| PauseResumeStream
+
+type Roomba = {
+    OperatingMode: OperatingMode
+}
+
+let getCommandData command =
+    match command with
+    | Start -> { OpCode = 128; DataBytes = 0 }
+    | Reset -> { OpCode =   7; DataBytes = 0 }
+    | Stop  -> { OpCode = 173; DataBytes = 0 }
+    | Baud  -> { OpCode = 129; DataBytes = 1 }
+
+let getOperatingModeCommandData operatingMode = 
+    match operatingMode with 
+    | Safe -> { OpCode = 131; DataBytes = 0 }
+    | Full -> { OpCode = 132; DataBytes = 0 }
+    | _    -> failwith "not covered by manual!"
+
+let getCleaningModeCommandData cleaningMode =
+    match cleaningMode with 
+    | Spot       -> { OpCode = 134; DataBytes = 0  }
+    | Clean      -> { OpCode = 135; DataBytes = 0  }
+    | SeekDock   -> { OpCode = 143; DataBytes = 0  }
+    | Power      -> { OpCode = 133; DataBytes = 0  }
+    | Max        -> { OpCode = 136; DataBytes = 0  }
+    | Schedule   -> { OpCode = 167; DataBytes = 15 }
+    | SetDayTime -> { OpCode = 168; DataBytes = 3  }
+
+let getInputCommandData inputCommand =
+    match inputCommand with 
+    | Sensors           -> { OpCode = 142; DataBytes = 1 }
+    | QueryList         -> { OpCode = 149; DataBytes = 1 } // actually N + 1, where N is the number of packets requested. 
+    | Stream            -> { OpCode = 148; DataBytes = 1 } // actually N + 1, where N is the number of packets requested.
+    | PauseResumeStream -> { OpCode = 150; DataBytes = 1 }
+
+let sendCommand roomba command =
+    match command with 
+    | Start -> { roomba with OperatingMode = Passive }
+    | _     -> failwith "find something better than failwith"
+
+let sendModeCommand roomba command = 
+    match command with 
+    | _ -> { roomba with OperatingMode = Passive }
+
