92e8f5bfeb4fe83129eb5cab1b85878595e0fe34
Falkner, David <David.B.Falkner@nordstrom.com>
2017-10-26 22:03:37 -0700

    constructed data for SetDayTime command

--- main/CleaningMode.fs
+++ main/CleaningMode.fs
@@ -1,67 +1,71 @@
 namespace iRobot
 
-open Communicator
-open OperatingMode
+module CleaningMode =
 
-type CleaningMode =
-| Spot
-| Clean
-| SeekDock
-| Power
-| Max
-| Schedule
-| SetDayTime
+    open System
+    open Communicator
+    open OperatingMode
 
-module CleaningModeConstructors =
-    let getCleaningModeCommandData cleaningMode =
-        match cleaningMode with 
-        | Spot       -> { OpCode = 134uy; DataBytes = Array.empty  } //✓
-        | Clean      -> { OpCode = 135uy; DataBytes = Array.empty  } //✓
-        | SeekDock   -> { OpCode = 143uy; DataBytes = Array.empty  } //✓
-        | Power      -> { OpCode = 133uy; DataBytes = Array.empty  } //✓
-        | Max        -> { OpCode = 136uy; DataBytes = Array.empty  } //✓
-        | Schedule   -> { OpCode = 167uy; DataBytes = Array.empty  }
-        | SetDayTime -> { OpCode = 168uy; DataBytes = Array.empty  }
+    type CleaningMode =
+    | Spot       of CommandData
+    | Clean      of CommandData
+    | SeekDock   of CommandData
+    | Power      of CommandData
+    | Max        of CommandData
+    | Schedule   of CommandData
+    | SetDayTime of CommandData
 
-    let sendCleaningModeCommand roomba cleaningModeCommand =
-        let xmitCommand newMode =
-            cleaningModeCommand |> getCleaningModeCommandData |> transmitCommandData
+    let createSpot       = { OpCode = 134uy; DataBytes = Array.empty }
+    let createClean      = { OpCode = 135uy; DataBytes = Array.empty }
+    let createSeekDock   = { OpCode = 143uy; DataBytes = Array.empty }
+    let createPower      = { OpCode = 133uy; DataBytes = Array.empty }
+    let createMax        = { OpCode = 136uy; DataBytes = Array.empty }
+    let createSchedule   = { OpCode = 167uy; DataBytes = Array.empty }
+    let createSetDayTime (dateTime:DateTime) =
+        let data = 
+            [| int dateTime.DayOfWeek; dateTime.Hour; dateTime.Minute |]
+            |> Array.map byte
+        { OpCode = 168uy; DataBytes = data }
+
+    let sendCleaningModeCommand roomba (cleaningModeCommand:CleaningMode) =
+        let xmitCommand newMode commandData =
+            commandData |> transmitCommandData
             { roomba with OperatingMode = newMode }
         match roomba.OperatingMode with
         | Off ->
             match cleaningModeCommand with
-            | Spot       -> failwith "illegal"
-            | Clean      -> failwith "illegal"
-            | SeekDock   -> failwith "illegal"
-            | Power      -> failwith "illegal"
-            | Max        -> failwith "illegal"
-            | Schedule   -> failwith "illegal"
-            | SetDayTime -> failwith "illegal"
+            | Spot       _ -> failwith "illegal"
+            | Clean      _ -> failwith "illegal"
+            | SeekDock   _ -> failwith "illegal"
+            | Power      _ -> failwith "illegal"
+            | Max        _ -> failwith "illegal"
+            | Schedule   _ -> failwith "illegal"
+            | SetDayTime _ -> failwith "illegal"
         | Passive ->
             match cleaningModeCommand with
-            | Spot       -> xmitCommand Passive
-            | Clean      -> xmitCommand Passive
-            | SeekDock   -> xmitCommand Passive
-            | Power      -> xmitCommand Passive
-            | Max        -> xmitCommand Passive
-            | Schedule   -> xmitCommand roomba.OperatingMode
-            | SetDayTime -> xmitCommand roomba.OperatingMode
+            | Spot       cd -> xmitCommand Passive cd
+            | Clean      cd -> xmitCommand Passive cd
+            | SeekDock   cd -> xmitCommand Passive cd
+            | Power      cd -> xmitCommand Passive cd
+            | Max        cd -> xmitCommand Passive cd
+            | Schedule   cd -> xmitCommand roomba.OperatingMode cd
+            | SetDayTime cd -> xmitCommand roomba.OperatingMode cd
         | Safe _ ->
             match cleaningModeCommand with
-            | Spot       -> xmitCommand Passive
-            | Clean      -> xmitCommand Passive
-            | SeekDock   -> xmitCommand Passive
-            | Power      -> xmitCommand Passive
-            | Max        -> xmitCommand Passive
-            | Schedule   -> xmitCommand roomba.OperatingMode
-            | SetDayTime -> xmitCommand roomba.OperatingMode
+            | Spot       cd -> xmitCommand Passive cd
+            | Clean      cd -> xmitCommand Passive cd
+            | SeekDock   cd -> xmitCommand Passive cd
+            | Power      cd -> xmitCommand Passive cd
+            | Max        cd -> xmitCommand Passive cd
+            | Schedule   cd -> xmitCommand roomba.OperatingMode cd
+            | SetDayTime cd -> xmitCommand roomba.OperatingMode cd
         | Full _ ->
             match cleaningModeCommand with
-            | Spot       -> xmitCommand Passive
-            | Clean      -> xmitCommand Passive
-            | SeekDock   -> xmitCommand Passive
-            | Power      -> xmitCommand Passive
-            | Max        -> xmitCommand Passive
-            | Schedule   -> xmitCommand roomba.OperatingMode
-            | SetDayTime -> xmitCommand roomba.OperatingMode
+            | Spot       cd -> xmitCommand Passive cd
+            | Clean      cd -> xmitCommand Passive cd
+            | SeekDock   cd -> xmitCommand Passive cd
+            | Power      cd -> xmitCommand Passive cd
+            | Max        cd -> xmitCommand Passive cd
+            | Schedule   cd -> xmitCommand roomba.OperatingMode cd
+            | SetDayTime cd -> xmitCommand roomba.OperatingMode cd
 
