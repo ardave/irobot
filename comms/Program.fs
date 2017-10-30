6b2ba1f91b200fc56cac3bfe1570fe7c71d05ac8
David Falkner <ardave@gmail.com>
2017-10-29 18:30:28 -0700

    comm debugging

--- comms/Program.fs
+++ comms/Program.fs
@@ -1,7 +1,10 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open RJCP.IO.Ports
+
+let tryIt() =
+    let src = new SerialPortStream("/dev/ttyS0")
+    src.Dispose()
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    tryIt()
+    0
