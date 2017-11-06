c9a9200513c90e1380a1999625a60c2f20f8ff6c
Dave Falkner <ardave@gmail.com>
2017-11-05 16:33:36 -0800

    test project added.  Everything run fine on the robot.

---
+++ src/app/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+open MessageAgent
+open iRobot
+open iRobot.Comms
+
+[<EntryPoint>]
+let main _ =
+    printfn "Starting ..."
+    let byteReceived, writeBytes, disposableOpt = createConnection (Real("/dev/ttyUSB0"))
+    let messageAgent = createMessageAgent(writeBytes)
+
+    try
+        byteReceived (messageAgent.Post << ByteReceivedFromRobot)
+
+        let rec monitorKeyboardInput() =
+            let keyInfo = Console.ReadKey()
+            match keyInfo.Key with
+            | ConsoleKey.Q ->
+                printfn "Cancel key pressed."
+            | _ -> 
+                messageAgent.Post <| UserKeyPress(keyInfo)
+                monitorKeyboardInput()
+
+        monitorKeyboardInput()
+    finally
+        match disposableOpt with | Some d -> d.Dispose() | None -> ()
+    
+    0
